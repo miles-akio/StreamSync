a9d50ef0eab43cc4bed62d0172469bb78358d4fc
Miles Shinmachi <78616843+miles-akio@users.noreply.github.com>
2025-06-12 22:56:49 -0700

    Delete components directory

--- components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer style={{ padding: "1rem", backgroundColor: "#1e40af", color: "white", marginTop: "2rem" }}>
-      <p>© 2024 StreamSync. All rights reserved.</p>
-    </footer>
-  );
-}
